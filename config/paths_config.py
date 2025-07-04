eaa07aaf47e5c3fc5d9a5c37f32ea4431033d098
Ujjwal Maurya <ujjwalsbx@gmail.com>
2025-07-05 00:44:04 +0530

    config for processed data added

--- config/paths_config.py
+++ config/paths_config.py
@@ -8,3 +8,15 @@ TRAIN_FILE_PATH = os.path.join(RAW_DIR,"train.csv")
 TEST_FILE_PATH = os.path.join(RAW_DIR,"test.csv")
 
 CONFIG_PATH = "config/config.yaml"
+
+
+PROCESSED_DIR = "artifacts/processed"
+PROCESSED_TRAIN_FILE_PATH = os.path.join(PROCESSED_DIR,"processed_train.csv")
+PROCESSED_TEST_FILE_PATH = os.path.join(PROCESSED_DIR,"processed_test.csv")
+
+
+
+
+
+
+
